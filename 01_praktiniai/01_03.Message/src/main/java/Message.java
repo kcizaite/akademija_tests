1489516bf7b4e88fb64ea4606496e9672362cc85
Karolina Čižaitė <61018680+kcizaite@users.noreply.github.com>
2022-12-02 11:56:30 +0200

    Initial commit

---
+++ 01_praktiniai/01_03.Message/src/main/java/Message.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+
+        System.out.println("Write a message:");
+        // Write your program here
+        System.out.println(message);
+    }
+}
